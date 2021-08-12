2ed91d5ed004ba480348761c956cd8ef0e15ce39
Alexis Tricot <alexis.tricot@sysnav.fr>
2021-08-13 01:12:57 +0200

    Update the handling of plays.

--- handlePlay.js
+++ handlePlay.js
@@ -1,3 +1,18 @@
-module.exports = function() {
-    return;
+const config = require("./config.json");
+const dudo = require("./dudo");
+const calzone = require("./calzone");
+const newBet = require("./newBet");
+
+module.exports = function(message, Game) {
+    const content = message.content.trim().toLowerCase();
+    // check the message content for keywords
+    if (config["dudo"].includes(content)) {
+        dudo(Game);
+    }
+    else if (config["calzone"].includes(content)) {
+        calzone(Game);
+    }
+    else {
+        newBet(Game);
+    }
 };
