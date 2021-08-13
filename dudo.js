058307af3afbc8b7877cb70698dd077a7821a6ca
Alexis Tricot <alexis.tricot@sysnav.fr>
2021-08-13 15:50:02 +0200

    Add module to handle the dudo command

---
+++ dudo.js
@@ -0,0 +1,28 @@
+const config = require("./config.json");
+
+module.exports = function(message, Game) {
+    // check if the author of the message is the player whose turn it is to play
+    if (!(message.author.id === Game.player[Game.current].id)) return;
+    message.react(config["dodo"]);
+    // check if the player who dudoed won
+    let winner, loser;
+    if (Game.Bet.count() > Game.count[Game.Bet.dice()]) {
+        // the dudo won
+        winner = Game.current;
+        loser = Game.previousPlayer();
+    }
+    else {
+        // the dudo lost
+        winner = Game.previousPlayer();
+        loser = Game.current;
+    }
+    // notify the players of the results
+    Game.sendResults();
+    Game.channel.send(
+        `${Game.player[winner].toString()} won, ${Game.player[loser]} lost the round`,
+    );
+    // set the current player to the loser and remove a dice
+    Game.current = loser;
+    Game.removeDice(loser);
+    Game.startRound();
+};
